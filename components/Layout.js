5e394eec6263ee6c4f87df95eb1a5871bf45fd36
roman-overhaul <roman@romanhermens.com>
2024-04-13 11:59:53 -0700

    fixing cursor position

--- components/Layout.js
+++ components/Layout.js
@@ -8,12 +8,12 @@ export default function Layout({ children, preview, pageData }) {
   return (
     <div className={`h-full flex flex-col cursor-none`}>
         <MetaTags />
+        <Cursor/>
         <Header />
         <main className={`grow relative min-h-screen`}>
           {children}
         </main>
         <Footer />
-        <Cursor/>
     </div>
   );
 }
